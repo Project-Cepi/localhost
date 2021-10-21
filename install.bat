b41555067113d740afb93d76f12d780221f59b7c
Tristan <LeoDog896@hotmail.com>
2021-10-21 11:44:30 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@
 powershell -c "Invoke-WebRequest -Uri 'https://github.com/Project-Cepi/Sabre/releases/download/latest/sabre-1.0.0-all.jar' -OutFile 'sabre.jar'"
 
 @rem download import map
-powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Project-Cepi/import-map/main/import-map.json' -OutFile 'import-map.json'"
+powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Project-Cepi/localhost/main/import-map.json' -OutFile 'import-map.json'"
 
 echo @echo off > run.bat
 echo java -Xmx2024M -Xms2024M -jar sabre.jar >> run.bat

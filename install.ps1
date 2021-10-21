2acfc55e3249dc788f13e2f9818615615c91bf0f
Tristan <LeoDog896@hotmail.com>
2021-10-21 11:44:49 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # download jar
 iwr https://github.com/Project-Cepi/Sabre/releases/download/latest/sabre-1.0.0-all.jar -OutFile sabre.jar
 # download import map
-iwr https://raw.githubusercontent.com/Project-Cepi/import-map/main/import-map.json -OutFile import-map.json
+iwr https://raw.githubusercontent.com/Project-Cepi/localhost/main/import-map.json -OutFile import-map.json
 
 "java -Xmx2024M -Xms2024M -jar sabre.jar" > run.ps1
 

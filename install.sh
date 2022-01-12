80a2d41edbb42932ef0fb2d11e93f7ac4c6927c0
Tristan <LeoDog896@hotmail.com>
2022-01-11 19:31:26 -0500

    Green

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # download jar
-printf -- "Downloading Sabre Server Jar...\n"
+printf -- "\e[32mDownloading Sabre Server Jar...\e[m\n"
 wget https://github.com/Project-Cepi/Sabre/releases/download/latest/sabre-1.0.0-all.jar -O sabre.jar
 
 # download import map
-printf -- "Downloading import map (links to all extensions for downloading)\n"
+printf -- "\e[32mDownloading import map (links to all extensions for downloading)...\e[m\n"
 wget https://raw.githubusercontent.com/Project-Cepi/localhost/main/import-map.json -O import-map.json
 
-printf -- "Making run.sh file...\n"
+printf -- "\e[32mMaking run.sh file...\e[m\n"
 echo "#!/bin/bash
 java -Xmx2024M -Xms2024M -jar sabre.jar" > run.sh
 
 # give run.sh executable permissions
 chmod +x ./run.sh
 
-printf -- "Sucessfully installed sabre + cepi's import map. Use ./run.sh to run the jar and install extensions.\n"
+printf -- "\e[32mSucessfully installed sabre + cepi's import map. Use ./run.sh to run the jar and install extensions.\e[m\n"
